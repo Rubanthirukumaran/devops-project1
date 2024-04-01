a1e73f87eb890667a3e02a1b53c7167fd3acdbd4
unknown <x23163836@student.ncirl.ie>
2024-04-01 06:12:48 +0530

    2nd commit

--- project/urls.py
+++ project/urls.py
@@ -9,9 +9,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('food.urls')),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Add a newline at the end of the file
